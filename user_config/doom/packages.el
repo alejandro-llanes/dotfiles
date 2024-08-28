bbd64e4cca7fc3404bfe76e274c817613e8683a0
sombralibre <cllanes@lulobank.com>
2024-08-28 10:27:15 -0500

    fix doom-magit mac error

--- user_config/doom/packages.el
+++ user_config/doom/packages.el
@@ -56,3 +56,12 @@
 (package! ibuffer-sidebar)
 (package! lsp-ui)
 ;;(package! popwin)
+(when (eq system-type 'darwin)
+(package! transient
+      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+      :recipe (:host github :repo "magit/transient")))
+
+(when (eq system-type 'darwin)
+(package! with-editor
+          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+          :recipe (:host github :repo "magit/with-editor")))
