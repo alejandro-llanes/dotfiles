b6375f8eca2458d1f16e19c07debaad56fdf5241
Alejandro Llanes <sombra.libre@gmail.com>
2024-10-17 00:04:10 -0500

    add freebsd folder

---
+++ freebsd/user_config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias ls="lsd"
+    set hydro_color_pwd blue
+    set hydro_color_git green
+    set hydro_color_error red
+end
