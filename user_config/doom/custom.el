72cd311947fd9e66258d199603b917c328e9e3ce
Alejandro Llanes <sombra.libre@gmail.com>
2025-08-05 14:15:59 -0500

    pusha

--- user_config/doom/custom.el
+++ user_config/doom/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738"
+   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
+     "b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738"
      "5fa49528e5d7369c4ef8f55c02fb5aa54048bfa4f9b9795e9b3df0ec35221ac6"
      "dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176"
      "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default))
@@ -18,5 +19,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil)))))
 (put 'erase-buffer 'disabled nil)
